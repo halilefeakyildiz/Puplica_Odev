6614114c2277b7927bddbbe73fe1d34b641ad83d
Halil Efe Akyıldız <halilefeakyldz@gmail.com>
2025-04-27 17:07:30 +0300

    update2

---
+++ app/src/main/java/com/example/odevler/ui/theme/Odev_1.kt
@@ -0,0 +1,70 @@
+package com.example.odevler.ui.theme
+//Odev_1
+fun main(){
+    var sehir = "Istanbul"
+    var ulke = "Turkiye"
+    var telefon = "0511111111"
+    var posta_kod = 3451
+    var email = "kelykk@gmail.com"
+    var meslek = "Esnaf"
+    var stok = 53
+    var consumer_name = "Leyla"
+    var balance = 45650
+    var dg = "28/04/2004"
+    var maas = 150000
+    var medeni = "bekar"
+    var odeme_trh = "01/01/2096"
+    var araba = "Audi A5"
+    var kitap = "Kürk Mantolu Madonna"
+    var yayın_trh = "02/05/2012"
+    var enlem = 45
+    var boylam = 50
+    var urun_ad = "Apple Watch"
+    var muzik = "Cem Karaca - Sevda Kusun Kanadında"
+    var video_time = 56
+    var dosya = "pdf"
+    var renk = "Kırmızı"
+    var agirlik = 78
+    var ulusal_gun = "29/10/1923"
+    var sokak = "Cıkmaz Sokak"
+    var bus_hat = 210
+    var tr_code = 2513548285
+    var fatur_ad = "leySK/kunCD"
+
+    println(ulke)
+    println(telefon)
+    println(posta_kod)
+    println(email)
+    println(meslek)
+    println(stok)
+    println(consumer_name)
+    println(balance)
+    println(dg)
+    println(maas)
+    println(medeni)
+    println(odeme_trh)
+    println(araba)
+    println(kitap)
+    println(yayın_trh)
+    println(enlem)
+    println(boylam)
+    println(urun_ad)
+    println(muzik)
+    println(video_time)
+    println(dosya)
+    println(renk)
+    println(agirlik)
+    println(ulusal_gun)
+    println(sokak)
+    println(bus_hat)
+    println(tr_code)
+    println(fatur_ad)
+
+
+
+
+
+
+
+
+}
